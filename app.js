fba2834ca6e61318a8f60d9df253d442c97c1dc8
sergiomarben <sergio.martinbenitez@keifitech.com>
2021-04-22 09:22:29 +0200

    message run

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const configMessage = require('./config-message');
-
-
 const app = express();
+
 app.use(bodyParser.json());
+
 app.use(cors());
 
 app.post('/mail', (req, res) => {
     configMessage(req.body);
     res.status(200).send();
-})
+});
+
+app.get('/', (req, res) => {
+    res.send('mailer service run...');
+});
 
 const port = process.env.port || 3000;
 app.listen(port, () => {
-    console.log('Servidor corriendo')
+    console.log('Servidor corriendo');
 });
